591f4370a22c9927ed2e2083f13edcac2a376057
challenger1304 <v.neubauer@darlor.de>
2021-02-08 18:50:18 +0100

    Add migration and model for podcast episodes

---
+++ app/Models/PodcastEpisode.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class PodcastEpisode
+ *
+ * @author Vincent Neubauer <v.neubauer@vonmaehlen.com>
+ * @package App\Models
+ *
+ * @property string uuid
+ * @property int episode_number
+ * @property string title
+ * @property string|null subtitle
+ * @property string|null description
+ * @property string|null duration
+ * @property string|null type
+ * @property string|null image
+ * @property boolean explicit
+ * @property Carbon date_published
+ */
+class PodcastEpisode extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'uuid',
+        'episode_number',
+        'title',
+        'subtitle',
+        'description',
+        'duration',
+        'type',
+        'image',
+        'explicit',
+        'date_published',
+    ];
+}
---
+++ database/migrations/2021_02_08_142647_create_podcast_episodes_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePodcastEpisodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('podcast_episodes', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid');
+            $table->unsignedBigInteger('episode_number');
+            $table->string('title');
+            $table->string('subtitle')->nullable();
+            $table->string('description')->nullable();
+            $table->string('duration')->nullable();
+            $table->string('type')->nullable();
+            $table->string('image')->nullable();
+            $table->boolean('explicit')->default(false);
+            $table->timestamp('date_published')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('podcast_episodes');
+    }
+}
