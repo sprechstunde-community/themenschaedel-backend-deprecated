dc01492f77f0f83e5c528b248d08b82187ca9407
challenger1304 <v.neubauer@darlor.de>
2021-02-09 00:18:37 +0100

    Replace RSS parser with proper podcast feed parser

--- app/Jobs/ImportPodcastEpisodes.php
+++ app/Jobs/ImportPodcastEpisodes.php
@@ -3,15 +3,19 @@
 namespace App\Jobs;
 
 use App\Models\PodcastEpisode;
-use Carbon\Carbon;
-use Feed;
-use FeedException;
+use Exception;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Bus\Queueable;
 use Illuminate\Console\OutputStyle;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Lukaswhite\PodcastFeedParser\Artwork;
+use Lukaswhite\PodcastFeedParser\Episode;
+use Lukaswhite\PodcastFeedParser\Exceptions\InvalidXmlException;
+use Lukaswhite\PodcastFeedParser\Parser;
 
 class ImportPodcastEpisodes implements ShouldQueue
 {
@@ -32,14 +36,21 @@ class ImportPodcastEpisodes implements ShouldQueue
     /**
      * Execute the job.
      *
+     * @param Client $client
+     * @param Parser $parser
+     *
      * @return void
-     * @throws FeedException
+     * @throws GuzzleException
+     * @throws InvalidXmlException
+     * @throws Exception
      */
-    public function handle()
+    public function handle(Client $client, Parser $parser)
     {
+
         // download rss feed
-        $feed = Feed::loadRss(config('services.podcasts.rss_feed'));
-        $feedItems = $feed->item ?? [];
+        $response = $client->get(config('services.podcasts.rss_feed'));
+        $feed = $parser->setContent($response->getBody()->getContents())->run();
+        $feedItems = $feed->getEpisodes();
         $progress = null;
 
         // show progress
@@ -47,15 +58,17 @@ class ImportPodcastEpisodes implements ShouldQueue
             $progress = $this->output->createProgressBar(count($feedItems));
         }
 
-
-        foreach ($feedItems as $feedItem) {
+        /**
+         * @var Episode $feedItem
+         */
+        foreach ($feedItems->getIterator() as $feedItem) {
 
             // search for existing episodes with the same uuid
             /**
              * @var $episode PodcastEpisode
              */
             $episode = PodcastEpisode::firstOrNew([
-                'uuid' => $feedItem->guid,
+                'uuid' => $feedItem->getGuid(),
             ]);
 
             // only import if episode wasn't already imported
@@ -63,16 +76,16 @@ class ImportPodcastEpisodes implements ShouldQueue
 
                 // map feed to model
                 $episode->fill([
-                    'uuid' => (string) $feedItem->guid,
-                    'episode_number' => (int) $feedItem->{'itunes:episode'},
-                    'title' => (string) $feedItem->title,
-                    'subtitle' => (string) $feedItem->subtitle,
-                    'description' => (string) $feedItem->description,
-                    'image' => (string) $feedItem->{'itunes:image'},
-                    'type' => (string) $feedItem->{'itunes:episodeType'},
-                    'duration' => (string) $feedItem->{'itunes:duration'},
-                    'explicit' => $feedItem->{'itunes:explicit'} === 'no' ? false : true,
-                    'date_published' => new Carbon($feedItem->pubDate),
+                    'uuid' => $feedItem->getGuid(),
+                    'episode_number' => $feedItem->getEpisodeNumber(),
+                    'title' => $feedItem->getTitle(),
+                    'subtitle' => $feedItem->getSubtitle(),
+                    'description' => $feedItem->getDescription(),
+                    'image' => $feedItem->getArtwork() instanceof Artwork ? $feedItem->getArtwork()->getUri() : null,
+                    'type' => $feedItem->getType(),
+                    'duration' => $feedItem->getDuration(),
+                    'explicit' => $feedItem->getExplicit() === 'no' ? false : true,
+                    'date_published' => $feedItem->getPublishedDate(),
                 ]);
 
                 // persist model in database
