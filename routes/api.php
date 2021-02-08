065cb2737fa5d56afcbe962bf5c249f06314ef37
challenger1304 <v.neubauer@darlor.de>
2021-02-08 22:51:49 +0100

    Register api methods to manage episodes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\PodcastEpisodeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('episodes', PodcastEpisodeController::class)
+    ->parameters([
+        'episodes' => 'podcast_episode',
+    ]);
