08fed821f664ede11058ca29b839afdf53ee97c1
challenger1304 <v.neubauer@darlor.de>
2021-02-11 22:02:41 +0100

    Implement database seeder to easily generate testing data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,14 @@
 
 namespace Database\Seeders;
 
+use App\Jobs\ImportPodcastEpisodes;
+use App\Models\Claim;
+use App\Models\Episode;
+use App\Models\Flag;
+use App\Models\Subtopic;
+use App\Models\Topic;
+use App\Models\User;
+use App\Models\Vote;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +21,38 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $stdout = $this->command->getOutput();
+
+        // Import all podcast episodes from feed
+        dispatch_now(new ImportPodcastEpisodes($stdout));
+        $episodeCount = Episode::all()->count();
+        if ($stdout->isVerbose()) $stdout->text(sprintf('Imported %d episodes from rss feed', $episodeCount));
+
+        // Generate some users
+        $userCount = 50;
+        if ($stdout->isVerbose()) $stdout->text(sprintf('Generating %d users', $userCount));
+        User::factory($userCount)->create();
+
+        // Generate topics to episodes with each up to 5 subtopics
+        $topicCount = $episodeCount * 2;
+        if ($stdout->isVerbose()) $stdout->text(sprintf('Generating %d topics with subtopics', $topicCount));
+        Topic::factory($episodeCount)
+            ->has(Subtopic::factory(random_int(0, 5)))
+            ->create();
+
+        // Generate random votes for each episode
+        $voteCount = $episodeCount * 3;
+        if ($stdout->isVerbose()) $stdout->text(sprintf('Generating %d votes', $voteCount));
+        Vote::factory($voteCount)->create();
+
+        // Generate flags for some episodes
+        $flagCount = ceil($episodeCount > 20 ? $episodeCount / 10 : $episodeCount / 2);
+        if ($stdout->isVerbose()) $stdout->text(sprintf('Flagging %d episodes', $flagCount));
+        Flag::factory($flagCount)->create();
+
+        // Claiming episodes
+        $claimCount = ceil($userCount / 3);
+        if ($stdout->isVerbose()) $stdout->text(sprintf('Claiming %d episodes', $claimCount));
+        Claim::factory($claimCount);
     }
 }
