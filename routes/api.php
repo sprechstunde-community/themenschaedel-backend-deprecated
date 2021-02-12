394a336d01e6be88e11e3652c4e2f5f7731da606
challenger1304 <v.neubauer@darlor.de>
2021-02-12 23:36:36 +0100

    Define API routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\Api\EpisodeController;
+use App\Http\Controllers\Api\SubtopicController;
+use App\Http\Controllers\Api\TopicController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::apiResource('users', UserController::class);
+
 Route::apiResource('episodes', EpisodeController::class);
+
+Route::get('topics', [TopicController::class, 'index'])->name('topics.index');
+Route::apiResource('episodes.topics', TopicController::class)->shallow();
+
+Route::get('subtopics', [SubtopicController::class, 'index'])->name('subtopics.index');
+Route::apiResource('topics.subtopics', SubtopicController::class)->shallow();
