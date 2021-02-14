a58a2181241fc9340b9d89e5fb572ec06c497065
challenger1304 <v.neubauer@darlor.de>
2021-02-14 02:48:00 +0100

    Re-order api routes file

--- routes/api.php
+++ routes/api.php
@@ -20,19 +20,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// users
 Route::apiResource('users', UserController::class);
 
 // episodes
-Route::get('episodes/{episodes}/hosts', [HostController::class, 'indexScoped'])->name('episodes.hosts.index');
 Route::apiResource('episodes', EpisodeController::class);
+Route::get('episodes/{episodes}/hosts', [HostController::class, 'indexScoped'])->name('episodes.hosts.index');
+Route::post('episodes/{episode}/vote', [EpisodeController::class, 'vote'])->name('episodes.vote');
 
-// topics
-Route::get('topics', [TopicController::class, 'index'])->name('topics.index');
-Route::apiResource('episodes.topics', TopicController::class)->except(['index'])->shallow();
-Route::get('episodes/{episode}/topics', [TopicController::class, 'indexScoped'])->name('episodes.topics.index');
+// episode claims
 Route::post('episodes/{episode}/claim', [EpisodeClaimController::class, 'store'])->name('episodes.claim.store');
 Route::delete('episodes/{episode}/claim', [EpisodeClaimController::class, 'destroy'])->name('episodes.claim.destroy');
-Route::post('episodes/{episode}/vote', [EpisodeController::class, 'vote'])->name('episodes.vote');
 
 // flags
 Route::get('flags', [FlagController::class, 'index'])->name('flags.index');
@@ -42,7 +40,12 @@ Route::apiResource('episodes.flags', FlagController::class)->except(['index'])->
 // hosts
 Route::apiResource('hosts', HostController::class);
 
+// topics
+Route::apiResource('episodes.topics', TopicController::class)->except(['index'])->shallow();
+Route::get('topics', [TopicController::class, 'index'])->name('topics.index');
+Route::get('episodes/{episode}/topics', [TopicController::class, 'indexScoped'])->name('episodes.topics.index');
+
 // subtopics
-Route::get('subtopics', [SubtopicController::class, 'index'])->name('subtopics.index');
-Route::get('topics/{topic}/subtopics', [SubtopicController::class, 'indexScoped'])->name('topics.subtopics.index');
 Route::apiResource('topics.subtopics', SubtopicController::class)->except(['index'])->shallow();
+Route::get('topics/{topic}/subtopics', [SubtopicController::class, 'indexScoped'])->name('topics.subtopics.index');
+Route::get('subtopics', [SubtopicController::class, 'index'])->name('subtopics.index');
