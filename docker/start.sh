c23dc4c3454c02e133c175d93b06a3e029179afb
challenger1304 <v.neubauer@darlor.de>
2021-02-19 02:18:04 +0100

    Add scheduler and queue worker services

--- docker/start.sh
+++ docker/start.sh
@@ -10,8 +10,14 @@ sleep 2 # Wait for database to fully start
 php $WEBROOT/artisan migrate --force
 
 case "$1" in
+scheduler)
+    exec php $WEBROOT/artisan schedule:work
+    ;;
 webserver)
     exec apache2-foreground
+    ;;
+worker)
+    exec php $WEBROOT/artisan queue:work
     ;;
 *)
     echo "Please define which service to start" >&2
