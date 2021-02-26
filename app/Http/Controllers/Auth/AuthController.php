8b7131eb520b196db5dc3cade93183c0b2f69353
challenger1304 <v.neubauer@darlor.de>
2021-02-27 00:36:48 +0100

    Store oauth credentials ind db

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Models\OAuthCredentials;
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Config\Repository;
-use Illuminate\Http\Request;
+use Laravel\Socialite\Contracts\User as OAuthUser;
 use Laravel\Socialite\Facades\Socialite;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
@@ -11,15 +14,21 @@ class AuthController
 {
 
     private Repository $config;
+    private OAuthCredentials $credentialsRepository;
+    private User $userRepository;
 
     /**
      * AuthController constructor.
      *
      * @param Repository $config
+     * @param OAuthCredentials $credentialsRepository
+     * @param User $userRepository
      */
-    public function __construct(Repository $config)
+    public function __construct(Repository $config,  OAuthCredentials $credentialsRepository, User $userRepository)
     {
         $this->config = $config;
+        $this->credentialsRepository = $credentialsRepository;
+        $this->userRepository = $userRepository;
     }
 
     public function auth(string $provider): RedirectResponse
@@ -27,8 +36,30 @@ class AuthController
         return Socialite::driver($provider)->redirect();
     }
 
-    public function callback(Request $request, string $provider)
+    public function callback(string $provider)
     {
-        dd((array) Socialite::driver($provider)->stateless()->user());
+        // request information from auth provider
+        /** @var OAuthUser $oAuth */
+        $oAuth = Socialite::driver($provider)->stateless()->user();
+
+        // load existing credentials from database
+        /** @var OAuthCredentials $credentials */
+        $credentials = $this->credentialsRepository->firstOrNew([
+            'provider' => $provider,
+            'provider_id' => $oAuth->getId(),
+        ]);
+
+        // update (or insert) oauth credentials und user information
+        $credentials->forceFill([
+            'token' => $oAuth->token,
+            'refresh_token' => $oAuth->refreshToken ?? null,
+            'expires_at' => is_int($oAuth->expiresIn) ? Carbon::now()->addSeconds($oAuth->expiresIn) : null,
+            'user_information' => json_encode((array) $oAuth->user ?? null),
+        ]);
+
+        // for temporary testing TODO assign actual user model
+        $credentials->user()->associate(User::all()->random());
+
+        $credentials->save();
     }
 }
