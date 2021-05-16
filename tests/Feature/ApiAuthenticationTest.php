a8fb031db783967283771ad14c3160748d5a67f8
challenger1304 <v.neubauer@darlor.de>
2021-05-17 00:40:33 +0200

    Fix API authentication feature test

---
+++ tests/Feature/ApiAuthenticationTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Fortify\Actions\EnableTwoFactorAuthentication;
+use Tests\TestCase;
+
+class ApiAuthenticationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_users_can_authenticate()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->json('POST', 'https://' . $this->getDomain() . '/auth/login', [
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
+
+        $this->assertAuthenticated('web');
+        $response->assertOk();
+        $response->assertJson(['two_factor' => false]);
+    }
+
+    public function test_users_with_2fa_can_authenticate()
+    {
+        $user = User::factory()->create();
+        $this->app->make(EnableTwoFactorAuthentication::class)($user);
+        $recoveryKey = json_decode(decrypt($user->two_factor_recovery_codes))[0];
+        $user->save();
+
+        $response = $this->json('POST', 'https://' . $this->getDomain() . '/auth/login', [
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
+
+        $response->assertOk();
+        $response->assertJson(['two_factor' => true]);
+
+        $response = $this->json('POST', 'https://' . $this->getDomain() . '/auth/two-factor-challenge', [
+            'recovery_code' => $recoveryKey,
+        ]);
+
+        $response->assertSuccessful();
+        $this->assertAuthenticated('web');
+    }
+
+    public function test_users_can_not_authenticate_with()
+    {
+        $user = User::factory()->create();
+
+        $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'wrong-password',
+        ]);
+
+        $this->assertGuest();
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,9 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function getDomain(): string {
+        $domain = parse_url(config('app.url'), PHP_URL_HOST) ?? 'localhost';
+        return 'api.' . $domain;
+    }
 }
