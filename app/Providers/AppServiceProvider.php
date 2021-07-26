56bdbd0cc98757f751d05bb6d52fc149ee0de580
challenger1304 <v.neubauer@darlor.de>
2021-07-27 00:25:17 +0200

    Register guzzle client singleton

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\ClientInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton(ClientInterface::class, Client::class);
     }
 
     /**
