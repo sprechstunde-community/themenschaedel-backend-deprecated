fc4fdcf938c2eb628e414d640704d59be2666d79
dallyger <v.neubauer@darlor.de>
2021-10-02 15:39:42 +0200

    Ensure restriced API routes require authentication

--- routes/api.php
+++ routes/api.php
@@ -34,30 +34,37 @@ Route::get('search/topics', [SearchController::class, 'topics'])->name('search.t
 Route::get('search/subtopics', [SearchController::class, 'subtopics'])->name('search.subtopics');
 
 // episodes
-Route::apiResource('episodes', EpisodeController::class);
+Route::apiResource('episodes', EpisodeController::class)->only(['index', 'show']);
 Route::get('episodes/{episode}/hosts', [HostController::class, 'indexScoped'])->name('episodes.hosts.index');
-Route::post('episodes/{episode}/vote', [EpisodeController::class, 'vote'])->name('episodes.vote');
-
-// episode claims
-Route::post('episodes/{episode}/claim', [EpisodeController::class, 'claim'])->name('episodes.claim.store');
-Route::delete('episodes/{episode}/claim', [EpisodeController::class, 'unclaim'])->name('episodes.claim.destroy');
 
 // flags
-Route::get('flags', [FlagController::class, 'index'])->name('flags.index');
+Route::apiResource('flags', FlagController::class)->only(['index', 'show']);
 Route::get('episodes/{episode}/flags', [FlagController::class, 'indexScoped'])->name('episodes.flags.index');
-Route::apiResource('episodes.flags', FlagController::class)->except(['index'])->shallow();
 
 // hosts
-Route::post('hosts/{host}/episodes/{episode}', [HostController::class, 'attachEpisode'])->name('hosts.episodes.attach');
-Route::delete('hosts/{host}/episodes/{episode}', [HostController::class, 'detachEpisode'])->name('hosts.episodes.detach');
-Route::apiResource('hosts', HostController::class);
+Route::apiResource('hosts', HostController::class)->only(['index', 'show']);
 
 // topics
-Route::apiResource('episodes.topics', TopicController::class)->except(['index'])->shallow();
-Route::get('topics', [TopicController::class, 'index'])->name('topics.index');
+Route::apiResource('topics', TopicController::class)->only(['index', 'show']);
 Route::get('episodes/{episode}/topics', [TopicController::class, 'indexScoped'])->name('episodes.topics.index');
 
 // subtopics
-Route::apiResource('topics.subtopics', SubtopicController::class)->except(['index'])->shallow();
+Route::apiResource('subtopics', SubtopicController::class)->only(['index', 'show']);
 Route::get('topics/{topic}/subtopics', [SubtopicController::class, 'indexScoped'])->name('topics.subtopics.index');
-Route::get('subtopics', [SubtopicController::class, 'index'])->name('subtopics.index');
+
+// restricted API endpoints, that require user to be authenticated
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::post('episodes/{episode}/claim', [EpisodeController::class, 'claim'])->name('episodes.claim.store');
+    Route::delete('episodes/{episode}/claim', [EpisodeController::class, 'unclaim'])->name('episodes.claim.destroy');
+
+    Route::post('episodes/{episode}/vote', [EpisodeController::class, 'vote'])->name('episodes.vote');
+
+    Route::post('hosts/{host}/episodes/{episode}', [HostController::class, 'attachEpisode'])->name('hosts.episodes.attach');
+    Route::delete('hosts/{host}/episodes/{episode}', [HostController::class, 'detachEpisode'])->name('hosts.episodes.detach');
+
+    Route::apiResource('episodes', EpisodeController::class)->except(['index', 'show']);
+    Route::apiResource('hosts', HostController::class)->except(['index', 'show']);
+    Route::apiResource('episodes.flags', FlagController::class)->except(['index', 'show'])->shallow();
+    Route::apiResource('episodes.topics', TopicController::class)->except(['index', 'show'])->shallow();
+    Route::apiResource('topics.subtopics', SubtopicController::class)->except(['index', 'show'])->shallow();
+});
