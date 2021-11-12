df9045b2df1bdec8cfa75a8716e9bf2b50943896
dallyger <v.neubauer@darlor.de>
2021-11-12 22:49:52 +0100

    Add docs for search endpoints

--- app/Http/Controllers/Api/SearchController.php
+++ app/Http/Controllers/Api/SearchController.php
@@ -13,7 +13,31 @@ use RuntimeException;
 class SearchController extends AbstractApiController
 {
     /**
-     * Search for specific
+     * Search for episodes
+     *
+     * @OA\Get(
+     *     path="/search/episodes",
+     *     tags={"episodes", "search"},
+     *     @OA\Parameter(name="page", in="query", @OA\Schema(type="integer")),
+     *     @OA\Parameter(name="per_page", in="query", @OA\Schema(type="integer")),
+     *     @OA\Parameter(name="q", in="query", description="Search string", required=true,
+     *         @OA\Schema(type="string",example="Lorem Ipsum")),
+     *     @OA\Response(
+     *         response="200",
+     *         description="Success",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="data", ref="#/components/schemas/EpisodeResourceCollection"),
+     *             @OA\Property(property="current_page", type="integer", example=1),
+     *             @OA\Property(property="last_page", type="integer", example=13),
+     *             @OA\Property(property="per_page", type="integer", example=25),
+     *             @OA\Property(property="total", type="integer", example=325),
+     *             @OA\Property(property="last_page_url", type="string"),
+     *             @OA\Property(property="next_page_url", type="string", nullable=true),
+     *             @OA\Property(property="prev_page_url", type="string", nullable=true),
+     *         )
+     *     )
+     * )
+     *
      * @param Request $request
      * @param Episode $repository
      *
@@ -24,11 +48,71 @@ class SearchController extends AbstractApiController
         return $this->search($request, $repository);
     }
 
+    /**
+     * Search for topics
+     *
+     * @OA\Get(
+     *     path="/search/topics",
+     *     tags={"topics", "search"},
+     *     @OA\Parameter(name="page", in="query", @OA\Schema(type="integer")),
+     *     @OA\Parameter(name="per_page", in="query", @OA\Schema(type="integer")),
+     *     @OA\Parameter(name="q", in="query", description="Search string", required=true,
+     *         @OA\Schema(type="string",example="Lorem Ipsum")),
+     *     @OA\Response(
+     *         response="200",
+     *         description="Success",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="data", ref="#/components/schemas/TopicCollection"),
+     *             @OA\Property(property="current_page", type="integer", example=1),
+     *             @OA\Property(property="last_page", type="integer", example=13),
+     *             @OA\Property(property="per_page", type="integer", example=25),
+     *             @OA\Property(property="total", type="integer", example=325),
+     *             @OA\Property(property="last_page_url", type="string"),
+     *             @OA\Property(property="next_page_url", type="string", nullable=true),
+     *             @OA\Property(property="prev_page_url", type="string", nullable=true),
+     *         )
+     *     )
+     * )
+     * @param Request $request
+     * @param Topic $repository
+     *
+     * @return LengthAwarePaginator
+     */
     public function topics(Request $request, Topic $repository)
     {
         return $this->search($request, $repository);
     }
 
+    /**
+     * Search for subtopics
+     *
+     * @OA\Get(
+     *     path="/search/subtopics",
+     *     tags={"subtopics", "search"},
+     *     @OA\Parameter(name="page", in="query", @OA\Schema(type="integer")),
+     *     @OA\Parameter(name="per_page", in="query", @OA\Schema(type="integer")),
+     *     @OA\Parameter(name="q", in="query", description="Search string", required=true,
+     *         @OA\Schema(type="string",example="Lorem Ipsum")),
+     *     @OA\Response(
+     *         response="200",
+     *         description="Success",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="data", ref="#/components/schemas/SubtopicCollection"),
+     *             @OA\Property(property="current_page", type="integer", example=1),
+     *             @OA\Property(property="last_page", type="integer", example=13),
+     *             @OA\Property(property="per_page", type="integer", example=25),
+     *             @OA\Property(property="total", type="integer", example=325),
+     *             @OA\Property(property="last_page_url", type="string"),
+     *             @OA\Property(property="next_page_url", type="string", nullable=true),
+     *             @OA\Property(property="prev_page_url", type="string", nullable=true),
+     *         )
+     *     )
+     * )
+     * @param Request $request
+     * @param Subtopic $repository
+     *
+     * @return LengthAwarePaginator
+     */
     public function subtopics(Request $request, Subtopic $repository)
     {
         return $this->search($request, $repository);
--- config/openapi.php
+++ config/openapi.php
@@ -3,6 +3,10 @@
 /**
  * @OA\Info(title="Themenschaedel", version="SNAPSHOT")
  *
+ * Tags, that should be displayed at the top:
+ * @OA\Tag(name="search")
+ * @OA\Tag(name="auth")
+ *
  * @OA\Components(
  *     @OA\SecurityScheme(
  *         securityScheme="bearerAuth",
